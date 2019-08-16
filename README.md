28a3e302604949f930730f65f3e6e21d9b706e9b
Luan Phan <luanphandinh@outlook.com>
2019-08-16 10:16:47 +0700

    enhance docker start script

--- README.md
+++ README.md
@@ -5,5 +5,13 @@ Starting docker containers for common stuffs: mysql, adminer, es, rabbitmq, ...
 
 # Start and running
 ```bash
+chmod +x docker.sh
+```
+Run all
+```bash
+./docker.sh all
+```
+Or run optional services
+```bash
 ./docker.sh mysql adminer es rabbitmq
 ```
--- docker.sh
+++ docker.sh
@@ -1,12 +1,27 @@
 #!/usr/bin/env bash
 
+compose() {
+	compose="docker-compose"
+	SERVICES=$@
+
+	for SERVICE in $SERVICES ; do
+		compose+=" -f docker/compose/$SERVICE"
+		if [[ $SERVICE != *".yaml"* ]]; then
+			compose+=".yaml"
+		fi
+	done
+
+	compose+=" up"
+	echo $compose
+}
+
 NAMES=$@
-compose="docker-compose"
+COMMAND=""
+if [ "$NAMES" == "all" ]; then
+	COMMAND="$(compose `ls docker/compose`)"
+else
+	COMMAND="$(compose $NAMES)"
+fi
 
-for NAME in $NAMES ; do
-    compose+=" -f docker/compose/$NAME.yaml"
-done
-compose+=" up"
-
-echo $compose
-$compose
+echo $COMMAND
+$COMMAND
