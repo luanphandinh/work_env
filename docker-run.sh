13a7e6049b7f3e2465ed0cd1a912ae1f94d8b07a
Luan Phan <luanphandinh@outlook.com>
2019-10-12 14:47:00 +0700

    Format script

--- docker-run.sh
+++ docker-run.sh
@@ -1,79 +1,74 @@
 #!/usr/bin/env bash
 
-info()
-{
-	echo ""
-	echo "Usage: $0 -e ENV -s PARAMS"
-	echo -e "\t-e e Environment: Possible values ['dev', 'test'], avoiding conflict data, accidentally delete dev data when running test"
-	echo -e "\t-s s Services: optional services to bootstrap"
-	exit 1
+info() {
+  echo ""
+  echo "Usage: $0 -e ENV -s PARAMS"
+  echo -e "\t-e e Environment: Possible values ['dev', 'test'], avoiding conflict data, accidentally delete dev data when running test"
+  echo -e "\t-s s Services: optional services to bootstrap"
+  exit 1
 }
 
-compose()
-{
-	compose="docker-compose"
-	PARAMS=$@
+compose() {
+  compose="docker-compose"
+  PARAMS=$@
 
-	for SERVICE in ${PARAMS}; do
-		compose+=" -f docker/compose/$SERVICE"
-		if [[ ${SERVICE} != *".yaml"* ]]; then
-			compose+=".yaml"
-		fi
-	done
+  for SERVICE in ${PARAMS}; do
+    compose+=" -f docker/compose/$SERVICE"
+    if [[ ${SERVICE} != *".yaml"* ]]; then
+      compose+=".yaml"
+    fi
+  done
 
-	compose+=" up"
-	echo "$compose"
+  compose+=" up"
+  echo "$compose"
 }
 
-command()
-{
-	PARAMS=$@
+command() {
+  PARAMS=$@
   SERVICES="all"
 
-	if [[ -n "$PARAMS" ]]; then
-	  FILTER=""
-	  for SERVICE in ${PARAMS}; do
+  if [[ -n "$PARAMS" ]]; then
+    FILTER=""
+    for SERVICE in ${PARAMS}; do
       FILTER+="-e $SERVICE.*yaml "
     done
 
     SERVICES=$(ls -p docker/compose | grep ${FILTER})
   fi
 
-	COMMAND=""
-	if [[ "$SERVICES" == "all" ]]; then
-		COMMAND="$(compose `ls docker/compose`)"
-	else
-		COMMAND="$(compose "$SERVICES")"
-	fi
+  COMMAND=""
+  if [[ "$SERVICES" == "all" ]]; then
+    COMMAND="$(compose $(ls docker/compose))"
+  else
+    COMMAND="$(compose "$SERVICES")"
+  fi
 
-	echo "$COMMAND"
-	${COMMAND}
+  echo "$COMMAND"
+  ${COMMAND}
 }
 
-env()
-{
-	ENV=$1
+env() {
+  ENV=$1
 
-	if [[ -z "$ENV" ]]; then
-		ENV="dev"
-	fi
+  if [[ -z "$ENV" ]]; then
+    ENV="dev"
+  fi
 
-	if [[ "$ENV" != "dev" ]] && [[ "$ENV" != "test" ]]; then
-		echo "-e only accept values ['dev', 'test']"
-		exit 1
-	fi
+  if [[ "$ENV" != "dev" ]] && [[ "$ENV" != "test" ]]; then
+    echo "-e only accept values ['dev', 'test']"
+    exit 1
+  fi
 
-	echo "Docker containers running on ENV: $ENV"
-	export ENV="$ENV"
+  echo "Docker containers running on ENV: $ENV"
+  export ENV="$ENV"
 }
 
-while getopts "e:s:h:" opt
-do
-   case "$opt" in
-      e ) ENV="$OPTARG" ;;
-      s ) PARAMS="$OPTARG" ;;
-      h ) info ;;
-   esac
+while getopts "e:s:h:" opt; do
+  case "$opt" in
+  e) ENV="$OPTARG" ;;
+  s) PARAMS="$OPTARG" ;;
+  h) info ;;
+  esac
 done
 
 env "$ENV"
