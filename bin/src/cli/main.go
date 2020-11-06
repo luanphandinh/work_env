1fe0a0ec1aed00c3d8a27152529e05b9b5490318
Luan Phan <luanphandinh@outlook.com>
2020-11-06 15:22:30 +0700

    Define basic structs

--- bin/src/cli/main.go
+++ bin/src/cli/main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+type CLIExecutable func(args []string) error
+
+type CLI struct {
+	Commands []CLIExecutable
+}
+
+type Command struct {
+	Name  string
+	Usage string
+	Exec  CLIExecutable
+}
+
 func main() {
 	fmt.Println("Hello cli v3, with gooooooooooo laaaaaaaaaaang")
 }
