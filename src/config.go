6c222a1233e419ac97237056c83e638072e0fda7
Luan Phan <luanphandinh@outlook.com>
2021-05-03 20:37:39 +0700

    Fix unit test for new custom config

--- src/config.go
+++ src/config.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"os"
 
@@ -10,7 +9,6 @@ import (
 )
 
 func getCurrentProfile(cli *CLI) string {
-	// return string(cli.GetConfig("current_profile").(string))
 	return cli.GetCustomConfig().(*Config).CurrentProfile
 }
 
@@ -23,8 +21,8 @@ func deleteProfile(cli *CLI) {
 	profile := cli.ShiftStrictArg()
 	profilesPath := util.GetDirPath(fmt.Sprintf("%s/%s", profileDir, profile))
 	if getCurrentProfile(cli) != "default" {
-		cli.SetConfig("current_profile", "default")
-		saveConfig(cli)
+		cli.GetCustomConfig().(*Config).SetCurrentProfile("default")
+		saveCustomConfig(cli)
 	}
 
 	err := cli.ExecCmd("rm", "-rf", profilesPath)
@@ -53,53 +51,11 @@ func loadCustomConfig(cli *CLI) error {
 	return nil
 }
 
-func loadConfig(cli *CLI) error {
-	cli.SetConfig("current_profile", "default")
-	data, err := util.GetFileContent(configDir, configFile)
-	if err != nil {
-		return err
-	}
-
-	if data == nil {
-		return errors.New("Invalid data")
-	}
-
-	var objmap map[string]json.RawMessage
-	err = json.Unmarshal(data, &objmap)
-	if err != nil {
-		return err
-	}
-
-	if currentProfile := objmap["current_profile"]; currentProfile != nil {
-		cli.SetConfig("current_profile", getJSONString(currentProfile))
-	}
-
-	if editor := objmap["editor"]; editor != nil {
-		cli.SetConfig("editor", getJSONString(editor))
-	}
-
-	return nil
-}
-
-func getJSONString(raw []byte) string {
-	data := raw[1 : len(raw)-1]
-	return string(data)
-}
-
 func saveCustomConfig(cli *CLI) {
 	file, err := os.Create(util.GetFilePath(configDir, configFile))
 	defer file.Close()
 	check(err)
 	config, _ := json.Marshal(cli.GetCustomConfig().(*Config))
-
-	file.Write(config)
-}
-
-func saveConfig(cli *CLI) {
-	file, err := os.Create(util.GetFilePath(configDir, configFile))
-	defer file.Close()
-	check(err)
-	config, _ := json.Marshal(cli.GetConfigs())
 
 	file.Write(config)
 }
